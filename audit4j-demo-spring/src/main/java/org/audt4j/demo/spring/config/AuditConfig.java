796de1fce679593aad7a728bf1f7f8f01dff68f8
Edward P. Legaspi <czetsuya@gmail.com>
2017-04-27 16:07:12 +0800

    Bump version 2.4.1. Use postgresql as database handler.

--- audit4j-demo-spring/src/main/java/org/audt4j/demo/spring/config/AuditConfig.java
+++ audit4j-demo-spring/src/main/java/org/audt4j/demo/spring/config/AuditConfig.java
@@ -1,14 +1,6 @@
 package org.audt4j.demo.spring.config;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import org.audit4j.core.handler.ConsoleAuditHandler;
-import org.audit4j.core.handler.Handler;
-import org.audit4j.core.layout.SimpleLayout;
-import org.audit4j.handler.db.DatabaseAuditHandler;
 import org.audit4j.integration.spring.AuditAspect;
-import org.audit4j.integration.spring.SpringAudit4jConfig;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
@@ -26,27 +18,27 @@ public class AuditConfig {
     // If you want to load configurations from file
     // (resources/audit4j.conf.yaml),
     // comment below method
-    @Bean
-    public DatabaseAuditHandler databaseHandler() {
-        DatabaseAuditHandler dbHandler = new DatabaseAuditHandler();
-        dbHandler.setEmbedded("true");
-        return dbHandler;
-    }
+//    @Bean
+//    public DatabaseAuditHandler databaseHandler() {
+//        DatabaseAuditHandler dbHandler = new DatabaseAuditHandler();
+//        dbHandler.setEmbedded("true");
+//        return dbHandler;
+//    }
 
     // If you want to load configurations from file
-    // (resources/audit4j.conf.yaml),
+	// (resources/audit4j.conf.yaml),
     // comment below method
-    @Bean
-    public SpringAudit4jConfig springAudit4jConfig() {
-        SpringAudit4jConfig springAudit4jConfig = new SpringAudit4jConfig();
-        springAudit4jConfig.setLayout(new SimpleLayout());
-        List<Handler> handlers = new ArrayList<Handler>();
-        handlers.add(new ConsoleAuditHandler());
-
-        handlers.add(databaseHandler());
-        springAudit4jConfig.setHandlers(handlers);
-        springAudit4jConfig.setMetaData(new AuditMetaData());
-        return springAudit4jConfig;
-    }
+	// @Bean
+	// public SpringAudit4jConfig springAudit4jConfig() {
+	// SpringAudit4jConfig springAudit4jConfig = new SpringAudit4jConfig();
+	// springAudit4jConfig.setLayout(new SimpleLayout());
+	// List<Handler> handlers = new ArrayList<Handler>();
+	// handlers.add(new ConsoleAuditHandler());
+	//
+	// handlers.add(databaseHandler());
+	// springAudit4jConfig.setHandlers(handlers);
+	// springAudit4jConfig.setMetaData(new AuditMetaData());
+	// return springAudit4jConfig;
+	// }
 
 }
